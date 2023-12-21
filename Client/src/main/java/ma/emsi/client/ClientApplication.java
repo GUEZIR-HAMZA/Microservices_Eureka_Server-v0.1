20e5ae64e542250680abd0f4c0225a97f7f367a4
GUEZIR-HAMZA <gr.hamza.99@gmail.com>
2023-12-21 23:21:47 +0100

    Initial commit

---
+++ Client/src/main/java/ma/emsi/client/ClientApplication.java
@@ -0,0 +1,18 @@
+package ma.emsi.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientApplication.class, args);
+	}
+
+
+}
